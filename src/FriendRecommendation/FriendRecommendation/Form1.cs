bb0ef8869d17093ba972edba69768d808dd11a5e
fwznbg <60412952+fwznbg@users.noreply.github.com>
2021-03-18 14:18:06 +0700

    gui

--- src/FriendRecommendation/FriendRecommendation/Form1.cs
+++ src/FriendRecommendation/FriendRecommendation/Form1.cs
@@ -6,15 +6,41 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace FriendRecommendation
 {
     public partial class Form1 : Form
     {
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+
+        private static extern IntPtr CreateRoundRectRgn
+         (
+               int nLeftRect,
+               int nTopRect,
+               int nRightRect,
+               int nBottomRect,
+               int nWidthEllipse,
+               int nHeightEllipse
+
+         );
+
         public Form1()
         {
             InitializeComponent();
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 25, 25));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //StreamWriter sw = new StreamWriter(Application.StartupPath + "\\Graph\\" + FirstName); // .txt path file
         }
     }
 }
