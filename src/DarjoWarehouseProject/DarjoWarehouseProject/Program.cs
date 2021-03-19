cf080437accdd0859667e331d556143692a8de31
fwznbg <60412952+fwznbg@users.noreply.github.com>
2021-03-19 21:01:34 +0700

    add visualizer

--- src/DarjoWarehouseProject/DarjoWarehouseProject/Program.cs
+++ src/DarjoWarehouseProject/DarjoWarehouseProject/Program.cs
@@ -6,17 +6,63 @@ using System.Windows.Forms;
 
 namespace DarjoWarehouseProject
 {
+    public class Visualizer
+    {
+        private Microsoft.Msagl.GraphViewerGdi.GViewer viewer;
+        private Microsoft.Msagl.Drawing.Graph graph;
+        public Visualizer()
+        {
+            viewer = new Microsoft.Msagl.GraphViewerGdi.GViewer();
+            graph = new Microsoft.Msagl.Drawing.Graph("graph");
+        }
+        public Microsoft.Msagl.GraphViewerGdi.GViewer Viewer
+        {
+            get { return viewer; }
+        }
+        public Microsoft.Msagl.Drawing.Graph Graph
+        {
+            get { return graph; }
+        }
+
+        public void Initialize(List<string> account, List<string[]> relations, int nRelations, Panel panel)
+        {
+            foreach (var r in relations)
+            {
+                graph.AddEdge(r[0], r[1]);
+            }
+
+            foreach (var n in account)
+            {
+                graph.FindNode(n).Attr.FillColor = Microsoft.Msagl.Drawing.Color.White;
+            }
+
+            viewer.Graph = graph;
+            graph.Attr.BackgroundColor = Microsoft.Msagl.Drawing.Color.MidnightBlue;
+            //graph.Attr.Border = 0;
+
+            panel.SuspendLayout();
+            viewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            panel.Controls.Add(viewer);
+            panel.ResumeLayout();
+
+        }
+
+    }
+
     static class Program
     {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
+        
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            
         }
     }
 }
