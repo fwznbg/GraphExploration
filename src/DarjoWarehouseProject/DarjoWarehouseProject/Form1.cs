b48a3add670bfa3d2896e8b0e4ca938b1994ae79
Afif Akromi <massachusetts48@gmail.com>
2021-03-25 11:26:57 +0700

    integrasi DFS

--- src/DarjoWarehouseProject/DarjoWarehouseProject/Form1.cs
+++ src/DarjoWarehouseProject/DarjoWarehouseProject/Form1.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
@@ -227,7 +223,7 @@ namespace DarjoWarehouseProject
                 }
                 else
                 {
-                    
+                    friendRecomendationResult = g.DFSFriendRecommendation();
                 }
 
                 // sort recommendation based on mutuals 
